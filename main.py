32faea3fb9bcd911a9baa06b8da8d585af497b37
HP <tesamuel070@gmail.com>
2023-04-05 11:03:47 -0700

    week2 code

---
+++ main.py
@@ -0,0 +1,11 @@
+"""in this week let us see the boolian data type"""
+print(20>23) #this program is used to show or print the boolian it says true or false
+print(45<342)
+
+# This is the IF statment
+a=19
+b=34
+if b<a: # "IF" means the statment is true
+    print("this is true b is lessthan a")
+else:  # "ELSE" means the statment is false
+    print("b is not lessthan a")
